916d352609603300c6f7ca7d3f1d8e42b518e0e4
Willian Marioti <willian.marioti@gmail.com>
2023-03-08 17:57:19 -0300

    Inclusão de verificação de consoante e string espelhada.

--- ManipulacaoDeString1/Program.cs
+++ ManipulacaoDeString1/Program.cs
@@ -4,9 +4,10 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string nome, nome2;
+        string nome, nome2, troca, mudar, mudar2, resu;
         int contador = 0;
-        int resultado;
+        int contador2 = 0;
+        int resultado, resultado2;
 
         Console.Write("Informe o nome: ");
         nome = Console.ReadLine();
@@ -14,6 +15,8 @@ internal class Program
         Console.WriteLine("\nForam informados {0} caracteres.", nome.Length);
 
         resultado = Vogais(nome);
+        resultado2 = Consoantes(nome);
+        mudar = Mudarstring(nome);
 
         int Vogais(string nome)
         {
@@ -29,8 +32,35 @@ internal class Program
             return contador;
         }
 
-        Console.WriteLine("A string possui {0} vogais.", resultado);
+        int Consoantes(string nome)
+        {
+            
 
+            for (int i = 0;i < nome.Length;i++)
+            {
+                char[] r = new char[nome.Length];
+                if (nome[i] != 'a' && nome[i] != 'e' && nome[i] != 'i'
+                && nome[i] != 'o' && nome[i] != 'u')
+                {
+                    r[i] = nome[i];
+                }
+                if (char.IsLetter(r[i]))
+                {
+                    contador2++;
+                }
+            }
+            return contador2;
+        }
+
+        string Mudarstring(string nome)
+        {
+            troca = new string(nome.Reverse().ToArray());
+            return troca;
+        }
+
+        Console.WriteLine("A string possui {0} vogais.", resultado);
+        Console.WriteLine("A string possui {0} consoantes.", resultado2);
+        Console.WriteLine("String ao contrário: {0}", mudar);
 
         Console.Write("\nInforme o nome 2: ");
         nome2 = Console.ReadLine();
@@ -45,5 +75,6 @@ internal class Program
             {
                 Console.WriteLine("As strings não são iguais!");
             }
+            
         }
     }
